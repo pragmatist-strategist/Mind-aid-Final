db908873af211bcd97e20e5dd5ed4789b64293cc
Pranav Sharma <ps20maths@gmail.com>
2019-07-24 19:18:18 +0530

    Final commit

---
+++ app/src/main/java/com/example/android/myapplication/BoardingActivity.java
@@ -0,0 +1,66 @@
+package com.example.android.myapplication;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.codemybrainsout.onboarder.AhoyOnboarderActivity;
+import com.codemybrainsout.onboarder.AhoyOnboarderCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardingActivity extends AhoyOnboarderActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AhoyOnboarderCard ahoyOnboarderCard1 = new AhoyOnboarderCard("Emotion Sensor", "Quantify emotions from voice using Deep Learning.", R.drawable.smile);
+        AhoyOnboarderCard ahoyOnboarderCard2 = new AhoyOnboarderCard("Mood Detection", "Facial emotion detection using Azure.", R.drawable.masks);
+        AhoyOnboarderCard ahoyOnboarderCard3 = new AhoyOnboarderCard("Anonymous Chat", "Speak your heart and vent out your emotions.", R.drawable.conversation);
+        AhoyOnboarderCard ahoyOnboarderCard4 = new AhoyOnboarderCard("Many features", "Some add-on features such as Games, Music and memes.", R.drawable.reward);
+
+
+
+        ahoyOnboarderCard1.setBackgroundColor(R.color.black_transparent);
+        ahoyOnboarderCard2.setBackgroundColor(R.color.black_transparent);
+        ahoyOnboarderCard3.setBackgroundColor(R.color.black_transparent);
+        ahoyOnboarderCard4.setBackgroundColor(R.color.black_transparent);
+
+
+
+        List<AhoyOnboarderCard> pages = new ArrayList<>();
+
+        pages.add(ahoyOnboarderCard1);
+        pages.add(ahoyOnboarderCard2);
+        pages.add(ahoyOnboarderCard3);
+
+        for (AhoyOnboarderCard page : pages) {
+            page.setTitleColor(R.color.white);
+            page.setDescriptionColor(R.color.grey_200);
+        }
+
+        setFinishButtonTitle("Get Started");
+        showNavigationControls(true);
+        //setGradientBackground();
+        setImageBackground(R.drawable.board3);
+
+//        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Light.ttf");
+        //setFont(face);
+
+        setInactiveIndicatorColor(R.color.grey_600);
+        setActiveIndicatorColor(R.color.white);
+
+        setOnboardPages(pages);
+
+    }
+
+    @Override
+    public void onFinishButtonPressed() {
+//        Toast.makeText(this, "Finish Pressed", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(getBaseContext(),SplashActivity.class));
+    }
+}
